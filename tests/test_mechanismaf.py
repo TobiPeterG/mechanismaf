495c132015686ca3c0b90e4c5bf2c01815294a27
TobiPeterG <github.threefold020@passmail.net>
2025-02-04 21:02:12 +0100

    FIx mechanismaf test
    Signed-off-by: TobiPeterG <github.threefold020@passmail.net>

--- tests/test_mechanismaf.py
+++ tests/test_mechanismaf.py
@@ -3,10 +3,10 @@ from mechanismaf import create_linkage_from_spec
 
 def test_create_linkage_from_spec():
     spec = [
-        ["bar", (0, 0), (1, 1), {"style": "ground"}],
-        ["bar", (1, 1), (2, 0)],
-        ["bar", (2, 0), (1, -1), {"angle_sweep": (0, 0, 1)}],
-        ["bar", (1, -1), (0, 0)],
+        ["bar", (0, 0), (0, 1), {"style": "ground"}],
+        ["bar", (0, 1), (1, 1), {"angle_sweep": (20, -20, 20)}],
+        ["bar", (1, 1), (1, 0)],
+        ["bar", (1, 0), (0, 0)],
     ]
     mech = create_linkage_from_spec(spec)
     assert mech is not None
